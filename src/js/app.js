e5cb492d3c97766851e813e7c263e8e7fb6b5e45
Martin <mdsugarev@gmail.com>
2021-12-24 16:50:54 +0200

    aaa

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,11 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  let content = document.querySelectorAll("p.subtitle.is-6.price.hot"); 
+         for(let i = 0; i < content.length; i++){
+          let price = content[i].innerHTML;
+          content[i].innerHTML = price + ' ' + '&#128293'
+         }
 });
+
+
